d8a509cc3507be510e4a4e4b44c03d26d93da3a3
1802ben <bwhite1802@gmail.com>
2025-05-30 10:50:15 -0500

    copy content from static folder to public folder

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from textnode import *
+from textnode import TextType, TextNode
+import os
+import shutil
+
 
 print("hello world")
 
@@ -7,5 +10,27 @@ def main():
     TestNode = TextNode("This is some anchor text",
                          TextType.LINK, "https://www.site.dev")
     print(repr(TestNode))
+    sync_directories("static", "public")
+
+def sync_directories(src: str, dst: str):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+
+    os.makedirs(dst, exist_ok=True)
+
+    def recursive_copy(current_src, current_dst):
+        for item in os.listdir(current_src):
+            s_path = os.path.join(current_src, item)
+            d_path = os.path.join(current_dst, item)
+            if os.path.isdir(s_path):
+                os.makedirs(d_path, exist_ok=True)
+                recursive_copy(s_path, d_path)
+            else:
+                shutil.copy2(s_path, d_path)
+
+    recursive_copy(src, dst)
+
+
+
 
 main()
