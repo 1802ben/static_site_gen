b360284f6ae4863d8ec636ad3e0395a1a4599fa3
1802ben <bwhite1802@gmail.com>
2025-06-02 10:16:18 -0500

    fix img

--- src/main.py
+++ src/main.py
@@ -3,18 +3,20 @@ import shutil
 import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page, generate_page_recursive
+from gencontent import generate_pages_recursive
+
 
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
+
 
 def main():
-    if sys.argv[0] != "": 
-        basepath = sys.argv[0]
-    else:
-        basepath = "/"
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
@@ -23,8 +25,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating pages from content directory...")
-    generate_page_recursive(dir_path_content, template_path, dir_path_public, basepath)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
